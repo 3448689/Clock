6525bbb20e6a0cbb43209d74ac22c5e100ce6d86
3448689 <47684182+3448689@users.noreply.github.com>
2023-06-07 02:57:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "index.html";
+const offlineFallbackPage = "https://github.com/3448689/Clock/blob/main/index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
